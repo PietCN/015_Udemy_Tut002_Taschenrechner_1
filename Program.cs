7a72b622d656c7b7ccd5615dccb4949dd93feb8a
Patrick <progTutLearn@outlook.com>
2016-11-27 14:14:14 +0100

    MethodeWarteAufBenutzerEingabe entfernt und dafür die Methode HoleBenutzerEingabe verwendet. Name der Lokalen Variablen der Methoe HoleBenutzerEingabe auf zahlAlsString geändert.

--- Program.cs
+++ Program.cs
@@ -4,29 +4,29 @@ namespace Taschenrechner
 {
   class Program
   {
-    static string HoleSummanden(string ausgabeText)
+    static string HoleBenutzerEingabe(string ausgabeText)
     {
       Console.Write(ausgabeText);
-      string summand =  Console.ReadLine();
-      return summand;
+      string zahlAlsString =  Console.ReadLine();
+      return zahlAlsString;
     }
 
     static void Main()
     {
       //User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
-      string ersterSummand = HoleSummanden("Bitte gebe den ersten Summanden ein: ");
-      string zweiterSummand = HoleSummanden("Bitte gebe den zweiten Summanden ein: ");
+      string ersteZahlAlsString = HoleBenutzerEingabe("Bitte gebe die erste Zahl ein: ");
+      string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gebe die zweite Zahl ein: ");
 
       //  Wandelt Text in Gleitkommazahlen um
       //  TODO: Auslagern in Methoden, wenn Struktur umfangreicher geworden ist.
-      double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
-      double zweiterSummandAlsZahl = Convert.ToDouble(zweiterSummand);
+      double ersterSummandAlsZahl = Convert.ToDouble(ersteZahlAlsString);
+      double zweiterSummandAlsZahl = Convert.ToDouble(zweiteZahlAlsString);
       //  Berechnung ausführen
       double summe = Addiere(ersterSummandAlsZahl, zweiterSummandAlsZahl);
 
       //  Ausgabe
       Console.WriteLine("Die Summe ist: {0}", summe);
-      WarteAufBenutzerEingabe();
+      HoleBenutzerEingabe("Zum beenden bitte Return drücken!");
     }
 
     static double Addiere(double summand1, double summand2)
@@ -38,11 +38,6 @@ namespace Taschenrechner
     {
       return minuend - subtrahent;
     }
-
-    static void WarteAufBenutzerEingabe()
-    {
-      Console.Write("Zum beenden bitte Return drücken!");
-      Console.ReadLine();
-    }
+    
   }
 }
