573be07b5367c37db5d9bf5503dc3395b3b0af37
Patrick <progTutLearn@outlook.com>
2016-11-28 16:06:57 +0100

    Die Methoden für die Multiplikation und Division mit eingepflegt. Auswahlbedingung über einer Switch-case realisiert.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Taschenrechner
       //User Story "Addieren": Als Benutzer möchte ich zwei Zahlen eingeben, um deren Summe berechnen zu lassen.
       string ersteZahlAlsString = HoleBenutzerEingabe("Bitte gebe die erste Zahl ein: ");
       string zweiteZahlAlsString = HoleBenutzerEingabe("Bitte gebe die zweite Zahl ein: ");
-      string operation = HoleBenutzerEingabe("Bitte gebe die auszuführende Operationein (+ oder -) : ");
+      string operation = HoleBenutzerEingabe("Bitte gebe die auszuführende Operationein (+, -, *, /) : ");
 
       //  Wandelt Text in Gleitkommazahlen um
       //  TODO: Auslagern in Methoden, wenn Struktur umfangreicher geworden ist.
@@ -25,24 +25,33 @@ namespace Taschenrechner
       double ergebnis = 0;
 
       //  Berechnung ausführen
-      if (operation == "+")
+      switch (operation)
       {
-        ergebnis = Addiere(zahl1, zahl2);
-      }
-      else if(operation == "-")
-      {
-        ergebnis = Subtrahiere(zahl1, zahl2);
-      }
-      else
-      {
-        Console.WriteLine("Du hast eine ungültige Auswahl oder Operation getroffen!");
+        case "+":
+          ergebnis = Addiere(zahl1, zahl2);
+          Console.WriteLine("Das Ergebnis ist: {0}", ergebnis);
+          break;
+        case "-":
+          ergebnis = Subtrahiere(zahl1, zahl2);
+          Console.WriteLine("Das Ergebnis ist: {0}", ergebnis);
+          break;
+        case "*":
+          ergebnis = Multiplizieren(zahl1, zahl2);
+          Console.WriteLine("Das Ergebnis ist: {0}", ergebnis);
+          break;
+        case "/":
+          ergebnis = Dividieren(zahl1, zahl2);
+          Console.WriteLine("Das Ergebnis ist: {0}", ergebnis);
+          break;
+        default:
+          Console.WriteLine("Du hast eine ungültige Auswahl oder Operation getroffen!");
+          break;
       }
 
-      //  Ausgabe
-      Console.WriteLine("Das Ergebnis ist: {0}", ergebnis);
       HoleBenutzerEingabe("Zum beenden bitte Return drücken!");
     }
 
+    //Mathematischen Operationsmethoden
     static double Addiere(double summand1, double summand2)
     {
       return summand1 + summand2;
@@ -53,5 +62,14 @@ namespace Taschenrechner
       return minuend - subtrahent;
     }
     
+    static double Multiplizieren(double faktor1, double faktor2)
+    {
+      return faktor1 * faktor2;
+    }
+
+    static double Dividieren(double divident, double divisor)
+    {
+      return divident / divisor;
+    }
   }
 }
